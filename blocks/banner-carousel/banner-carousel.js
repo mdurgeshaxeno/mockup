caa7c5022efef2758cf7668e85882ec89d0d3b94
mdurgeshaxeno <146161081+mdurgeshaxeno@users.noreply.github.com>
2023-10-16 21:05:06 +0530

    Create banner-carousel.js

---
+++ blocks/banner-carousel/banner-carousel.js
@@ -0,0 +1,34 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  ul.classList.add('owl-carousel', 'owl-theme', 'banner-carousel-list');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    li.innerHTML = row.innerHTML;
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(ul);
+
+  // Add the owl carousel script here
+  $(document).ready(function() {
+    var owl = $('.card-carousel-list');
+    owl.owlCarousel({
+      loop: true,
+      margin: 0,
+      items: 1,
+      dots: false,
+      nav: true,
+      autoplay: true,
+      autoplayTimeout: 1000,
+      autoplayHoverPause: true
+    });
+  });
+}
