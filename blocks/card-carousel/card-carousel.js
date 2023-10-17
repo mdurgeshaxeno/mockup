a02c770b11e9e0dbac45caff4a7f89595364eaee
mdurgeshaxeno <146161081+mdurgeshaxeno@users.noreply.github.com>
2023-10-17 09:22:03 +0530

    Update card-carousel.js

--- blocks/card-carousel/card-carousel.js
+++ blocks/card-carousel/card-carousel.js
@@ -1,43 +1,34 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  ul.classList.add('owl-carousel', 'owl-theme', 'card-carousel-list');
+  /* change to div */
+  const div = document.createElement('div');
+  div.classList.add('owl-carousel', 'owl-theme', 'banner-carousel-list');
   [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    li.innerHTML = row.innerHTML;
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+    const childDiv = document.createElement('div');
+    childDiv.innerHTML = row.innerHTML;
+    [...childDiv.children].forEach((innerDiv) => {
+      if (innerDiv.children.length === 1 && innerDiv.querySelector('picture')) innerDiv.className = 'cards-card-image';
+      else innerDiv.className = 'cards-card-body';
     });
-    ul.append(li);
+    div.append(childDiv);
   });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  div.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
-  block.append(ul);
+  block.append(div);
 
   // Add the owl carousel script here
   $(document).ready(function() {
-    var owl = $('.card-carousel-list');
+    var owl = $('.banner-carousel-list');
     owl.owlCarousel({
       loop: true,
-      margin: 10,
+      margin: 0,
+      items: 1,
       dots: false,
       nav: true,
-      autoplay: false,
-      responsiveClass: true,
-      responsive: {
-        0: {
-          items: 1
-        },
-        600: {
-          items: 2
-        },
-        1000: {
-          items: 4
-        }
-      }
+      autoplay: true,
+      autoplayTimeout: 5000,
+      autoplayHoverPause: true
     });
   });
 }
