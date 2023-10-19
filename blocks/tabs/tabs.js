90dbc405b8c10b65f61eaf37d4dddf388bda3b5a
mdurgeshaxeno <146161081+mdurgeshaxeno@users.noreply.github.com>
2023-10-19 10:03:42 +0530

    Update tabs.js

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -2,25 +2,42 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   /* change to div */
-  const div = document.createElement('div');
-  div.classList.add('owl-carousel', 'owl-theme', 'tabs-carousel-list');
-  [...block.children].forEach((row) => {
-    const childDiv = document.createElement('div');
-    childDiv.classList.add('tabs-carousel-item');
-    childDiv.setAttribute('data-dot', 'your-value-here');
-    childDiv.innerHTML = row.innerHTML;
-    [...childDiv.children].forEach((innerDiv, index) => {
-      if (index === 0) {
-        innerDiv.className = 'tabs-title';
-      } else {
-        innerDiv.className = 'tabs-body';
-      }
-    });
-    div.append(childDiv);
+const div = document.createElement('div');
+div.classList.add('owl-carousel', 'owl-theme', 'tabs-carousel-list');
+
+const dataDotValues = []; // Store data-dot values
+
+[...block.children].forEach((row) => {
+  const childDiv = document.createElement('div');
+  childDiv.classList.add('tabs-carousel-item');
+  childDiv.innerHTML = row.innerHTML;
+
+  const titleElements = [...childDiv.querySelectorAll('.tabs-title')];
+  
+  if (titleElements.length > 0) {
+    const dataDotValue = titleElements.map((titleElement) => titleElement.textContent).join(' '); // Concatenate the content of all 'tabs-title' elements
+    dataDotValues.push(dataDotValue);
+  }
+
+  [...childDiv.children].forEach((innerDiv, index) => {
+    if (index === 0) {
+      innerDiv.className = 'tabs-title';
+    } else {
+      innerDiv.className = 'tabs-body';
+    }
   });
-  div.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.textContent = '';
-  block.append(div);
+
+  div.append(childDiv);
+});
+
+// Set the 'data-dot' attribute to the concatenated values
+div.setAttribute('data-dot', dataDotValues.join(' '));
+
+div.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]));
+
+block.textContent = '';
+block.append(div);
+
 
   // Add the owl carousel script here
   $(document).ready(function () {
