0e91972dadead0ca6c0c174fb66e98921a54adaa
mdurgeshaxeno <146161081+mdurgeshaxeno@users.noreply.github.com>
2023-10-19 09:29:12 +0530

    Update tabs.js

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -4,20 +4,14 @@ export default function decorate(block) {
   /* change to div */
   const div = document.createElement('div');
   div.classList.add('owl-carousel', 'owl-theme', 'tabs-carousel-list');
-  [...block.children].forEach((row, index) => { // Add an index parameter to track the slide number
+  [...block.children].forEach((row) => {
     const childDiv = document.createElement('div');
     childDiv.classList.add('tabs-carousel-item');
     childDiv.innerHTML = row.innerHTML;
-    const firstColumnTitle = row.querySelector('.your-first-column-selector').innerText; // Replace with your first column selector
     [...childDiv.children].forEach((innerDiv) => {
-      if (innerDiv.children.length === 1 && innerDiv.querySelector('picture')) innerDiv.className = 'tabs-image';
+      if (innerDiv.children.length === 1) innerDiv.className = 'tabs-image';
       else innerDiv.className = 'tabs-body';
     });
-
-    // Set the data-dot attribute of the slide with the first column title
-    if (index === 0) {
-      childDiv.setAttribute('data-dot', firstColumnTitle);
-    }
     div.append(childDiv);
   });
   div.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
