9ca089497e290e2d7b4c7434f0226ba620e2ab05
mdurgeshaxeno <146161081+mdurgeshaxeno@users.noreply.github.com>
2023-10-19 10:23:14 +0530

    Update tabs.js

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -27,23 +27,12 @@ export default function decorate(block) {
     var owl = $('.tabs-carousel-list');
     owl.owlCarousel({
       loop: false,
-      margin: 10,
+      items: 1,
       dots: true,
       dotsData: true,
+      margin: 0,
       nav: false,
-      autoplay: false,
-      responsiveClass: true,
-      responsive: {
-        0: {
-          items: 1,
-        },
-        600: {
-          items: 1,
-        },
-        1000: {
-          items: 1,
-        },
-      },
+      autoplay: false
     });
   });
 }
