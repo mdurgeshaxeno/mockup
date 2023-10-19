41002251780a02cc2cba49b4d03f5ea9afd5f5c1
mdurgeshaxeno <146161081+mdurgeshaxeno@users.noreply.github.com>
2023-10-19 08:55:16 +0530

    Update tabs.js

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -1,26 +1,18 @@
 export default function decorate(block) {
-  /* change to div with class names */
-  const tabSection = document.createElement('div');
-  tabSection.className = 'tab-section';
-  
-  [...block.children].forEach((row) => {
-    const tabItem = document.createElement('div');
-    tabItem.className = 'tab-item';
-    while (row.firstElementChild) tabItem.append(row.firstElementChild);
-    
-    const divs = [...tabItem.children];
-    
-    // Check the number of divs and assign classes accordingly
-    if (divs.length === 2) {
-      divs[0].className = 'tab-head';
-      divs[1].className = 'tab-body';
-    }
+  const cols = [...block.firstElementChild.children];
+  block.classList.add(`columns-${cols.length}-cols`);
 
-    tabSection.append(tabItem);
+  // setup image columns
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+      const pic = col.querySelector('picture');
+      if (pic) {
+        const picWrapper = pic.closest('div');
+        if (picWrapper && picWrapper.children.length === 1) {
+          // picture is only content in column
+          picWrapper.classList.add('columns-img-col');
+        }
+      }
+    });
   });
-  
-  tabSection.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]));
-  
-  block.textContent = '';
-  block.append(tabSection);
 }
