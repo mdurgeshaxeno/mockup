4ea2ade8c24abb5cee26c1940dc2987f1e871ab2
mdurgeshaxeno <146161081+mdurgeshaxeno@users.noreply.github.com>
2023-10-19 22:58:27 +0530

    Update tabs.js

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -3,21 +3,29 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 export default function decorate(block) {
   /* change to div */
   const div = document.createElement('div');
-  div.classList.add('owl-carousel', 'owl-theme', 'tabs-carousel-list');
-  [...block.children].forEach((row) => {
-    const childDiv = document.createElement('div');
-    childDiv.classList.add('tabs-carousel-item');
-    childDiv.setAttribute('data-dot', 'your-value-here');
-    childDiv.innerHTML = row.innerHTML;
-    [...childDiv.children].forEach((innerDiv, index) => {
-      if (index === 0) {
-        innerDiv.className = 'tabs-title';
-      } else {
-        innerDiv.className = 'tabs-body';
-      }
-    });
-    div.append(childDiv);
+div.classList.add('owl-carousel', 'owl-theme', 'tabs-carousel-list');
+
+[...block.children].forEach((row) => {
+  const childDiv = document.createElement('div');
+  childDiv.classList.add('tabs-carousel-item');
+  
+  // Generate a random number and add it to 'your-value-here'
+  const randomNumber = Math.floor(Math.random() * 1000); // You can adjust the range as needed
+  const dataDotValue = `tab-${randomNumber}`;
+  
+  childDiv.setAttribute('data-dot', dataDotValue);
+  childDiv.innerHTML = row.innerHTML;
+
+  [...childDiv.children].forEach((innerDiv, index) => {
+    if (index === 0) {
+      innerDiv.className = 'tabs-title';
+    } else {
+      innerDiv.className = 'tabs-body';
+    }
   });
+
+  div.append(childDiv);
+});
   div.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(div);
