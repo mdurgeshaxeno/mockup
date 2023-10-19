3786b1060309048fb06ac479df30ded346c7e1bc
mdurgeshaxeno <146161081+mdurgeshaxeno@users.noreply.github.com>
2023-10-19 08:58:57 +0530

    Update tabs.js

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -8,16 +8,13 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     const tabItem = document.createElement('div');
     tabItem.className = 'tab-item';
-    while (row.firstElementChild) tabItem.append(row.firstElementChild);
-    
-    const divs = [...tabItem.children];
-    
-    // Check the number of divs and assign classes accordingly
-    if (divs.length === 2) {
-      divs[0].className = 'tab-head';
-      divs[1].className = 'tab-body';
+    while (row.firstElementChild) {
+      const div = document.createElement('div');
+      div.className = 'tab-content';
+      div.appendChild(row.firstElementChild);
+      tabItem.append(div);
     }
-
+    
     tabSection.append(tabItem);
   });
   
